baffdda677808aa8419f64878eb03a3d87eb873a
alexkarmhon <89stigmat89@gmail.com>
2023-12-15 15:59:41 +0200

    start Task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+player.on('play', function() {
+  console.log('played the video!');
+});
+
 
