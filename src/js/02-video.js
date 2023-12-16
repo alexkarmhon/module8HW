2d9392b899a685ad8c21059cccd436bdaaf05278
alexkarmhon <89stigmat89@gmail.com>
2023-12-16 16:11:54 +0200

    Task-2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,22 @@
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
-const player = new Player('vimeo-player', {
-    id: 19231868,
-    width: 640
-});
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+const CURRENT_TIME_KEY = 'video-player-current-time';
+const startPoint = localStorage.getItem(CURRENT_TIME_KEY) ?? 0;
 
+const getCurrentTime = function (videoData) {
+  const time = videoData.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(time));
+};
 
+player.setCurrentTime(startPoint);
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setVolume(0.1);
+player.setColor('#3289a8'); //set color of seeker
+player.setQuality('720p');
+// player.setMuted(true)
+// console.log(player.getQuality());
