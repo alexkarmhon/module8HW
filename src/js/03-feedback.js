0166986c1fe914e4b8c5da3ec7a1200862300e7f
alexkarmhon <89stigmat89@gmail.com>
2023-12-16 20:41:48 +0200

    Task-3 start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
 
+const EMAIL_STORAGE_KEY = 'EMAIL: ';
+const MESSAGE_STORAGE_KEY = 'MESSAGE: '
+// console.log(input);
+console.dir(textarea);
+
+textarea.innerText = "jhjhkjkjjkhkj"
+
+
+form.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('input', onMessageInput);
+
+function onMessageInput(e) {
+  const input = e.target;
+  
+
+  localStorage.setItem(MESSAGE_STORAGE_KEY, input.value)
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log('Submit');
+  form.reset();
+}
