a7a3d20d7b937c72ea9facde5dda24e45da7a611
alexkarmhon <89stigmat89@gmail.com>
2023-12-17 09:56:42 +0200

    fix
    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,21 +3,17 @@ const textarea = document.querySelector('.feedback-form textarea');
 const input = document.querySelector('.feedback-form input');
 
 const EMAIL_STORAGE_KEY = 'EMAIL: ';
-const MESSAGE_STORAGE_KEY = 'MESSAGE: '
-// console.log(input);
-console.dir(textarea);
+const MESSAGE_STORAGE_KEY = 'MESSAGE: ';
 
-textarea.innerText = "jhjhkjkjjkhkj"
-
+textarea.innerText = localStorage.getItem(MESSAGE_STORAGE_KEY) ?? '';
 
 form.addEventListener('submit', onFormSubmit);
 textarea.addEventListener('input', onMessageInput);
 
 function onMessageInput(e) {
   const input = e.target;
-  
 
-  localStorage.setItem(MESSAGE_STORAGE_KEY, input.value)
+  localStorage.setItem(MESSAGE_STORAGE_KEY, input.value);
 }
 
 function onFormSubmit(e) {
