e53a4f2fd9b16708a5600d233d061d6dc506fb1e
alexkarmhon <89stigmat89@gmail.com>
2023-12-17 11:54:46 +0200

    task-3 f

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,53 @@
+// const form = document.querySelector('.feedback-form');
+// const textarea = document.querySelector('.feedback-form textarea');
+// const input = document.querySelector('.feedback-form input');
+
+// const EMAIL_STORAGE_KEY = 'EMAIL: ';
+// const MESSAGE_STORAGE_KEY = 'MESSAGE: ';
+
+// textarea.value = localStorage.getItem(MESSAGE_STORAGE_KEY) ?? '';
+// input.value= localStorage.getItem(EMAIL_STORAGE_KEY) ?? '';
+
+// form.addEventListener('submit', onFormSubmit);
+// form.addEventListener('input', onInput);
+
+// function onInput(e) {
+
+//   if (e.target === textarea) {
+//     localStorage.setItem(MESSAGE_STORAGE_KEY, e.target.value);
+//   }
+
+//   if (e.target === input) {
+//     localStorage.setItem(EMAIL_STORAGE_KEY, e.target.value);
+//   }
+
+// }
+
+// function onFormSubmit(e) {
+//   // e.preventDefault();
+
+//   console.log('Submit');
+//   localStorage.removeItem(MESSAGE_STORAGE_KEY);
+//   localStorage.removeItem(EMAIL_STORAGE_KEY);
+//   form.reset();
+// }
+
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('.feedback-form textarea');
-const input = document.querySelector('.feedback-form input');
 
-const EMAIL_STORAGE_KEY = 'EMAIL: ';
-const MESSAGE_STORAGE_KEY = 'MESSAGE: ';
-
-textarea.innerText = localStorage.getItem(MESSAGE_STORAGE_KEY) ?? '';
+const formData = JSON.parse(localStorage.getItem("data")) ?? {};
 
 form.addEventListener('submit', onFormSubmit);
-textarea.addEventListener('input', onMessageInput);
+form.addEventListener('input', onInput);
 
-function onMessageInput(e) {
-  const input = e.target;
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
 
-  localStorage.setItem(MESSAGE_STORAGE_KEY, input.value);
+  localStorage.setItem('data', JSON.stringify(formData));
 }
 
 function onFormSubmit(e) {
-  e.preventDefault();
-
-  console.log('Submit');
+  localStorage.removeItem("data");
   form.reset();
 }
+
+console.log(form.elements.message.name);
